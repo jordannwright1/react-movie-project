54476ea77cd4e21ec6a4d81955a87af8e850ca72
Jordan <jordannwright1@gmail.com>
2025-04-26 13:43:51 -0400

    initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import "../css/Navbar.css"
+import { useNavigate } from "react-router-dom";
+
+function NavBar() {
+  const navigate = useNavigate();
+  const handleHomeClick = () => {
+    if (onHomeClick) onHomeClick();
+    navigate('/')
+  }
+  
+  return <nav className="navbar">
+    <div className="navbar-brand">
+      <Link to='/'>Movie App</Link>
+    </div>
+    <div className="navbar-links">
+    <a href="/" className="nav-link">Home</a>
+    <Link to='/favorites' className="nav-link">Favorites</Link>
+    </div>
+  </nav>
+}
+
+
+export default NavBar
